ad0b49e001927cb883e14337f3694ee363ad1c10
yamkim <coramdeo0708@gmail.com>
2021-09-07 13:42:42 +0900

    feat: [admin] 상품 관리 페이지 nav 바와 연동

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -6,8 +6,8 @@ const productController = require('../../controllers/admin/product');
 
 const uploadImage = require('../../libs/uploadImage')
 
-router.get('/', productController.renderProductManage);
-router.post('/', productController.displayProduct);
+router.get('/manage', productController.renderProductManage);
+router.post('/manage', productController.displayProduct);
 router.get('/register', productController.renderProductRegister);
 router.post('/register', uploadImage.single('thumbnail'), productController.registerProduct);
 
