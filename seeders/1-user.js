c3667663e792bed14aa4d4113c6a3075a8fa8a01
yamkim <coramdeo0708@gmail.com>
2021-09-14 00:10:50 +0900

    feat: [admin] userManage page 정렬 기능 추가

--- seeders/1-user.js
+++ seeders/1-user.js
@@ -4,27 +4,32 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         const emailForm = '@student.42seoul.kr';
+        const dates = [
+            new Date(2021, 3, 30),
+            new Date(2021, 3, 19),
+            new Date(2021, 4, 30),
+        ]
         const userSeed = [{
             id: 1,
             name: 'yekim',
             email: 'yekim' + emailForm,
             loginType: 1,
-            createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-            updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            createdAt: dates[0].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            updatedAt: dates[0].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
         }, {
             id: 2,
             name: 'yelee',
             email: 'yelee' + emailForm,
             loginType: 1,
-            createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-            updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            createdAt: dates[1].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            updatedAt: dates[1].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
         }, {
             id: 3,
             name: 'yekang',
             email: 'yekang' + emailForm,
             loginType: 1,
-            createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-            updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            createdAt: dates[2].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+            updatedAt: dates[2].toISOString().replace(/T/, ' ').replace(/\..+/, ''),
         }];
         await queryInterface.bulkInsert('users', userSeed, {});
     },
