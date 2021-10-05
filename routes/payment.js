c1684a451c3794ae2e9f6d4d6f8b7f8a488532c2
yamkim <coramdeo0708@gmail.com>
2021-10-05 10:25:54 +0900

    refactor: [server] sequelize reset을 위한 app.js 추가

--- routes/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const paymentController = require('../controllers/payment');
-
-router.post('/validation', paymentController.checkPayment);
-
-module.exports = router;
